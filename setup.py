e829d4245dc22f0f85dddc24f21ed8cf3495a933
Joe <joe@preferred.jp>
2022-02-23 18:16:28 +0900

    Run pytest and lint separately

--- setup.py
+++ setup.py
@@ -6,12 +6,5 @@ setup(
     packages=["crykak"],
     extras_require={
         "test": ["pytest"],
-        "lint": [
-            "pysen==0.10.1",
-            "black==21.10b0",
-            "flake8==4.0.1",
-            "isort==5.10.1",
-            "mypy==0.910",
-        ],
     },
 )
