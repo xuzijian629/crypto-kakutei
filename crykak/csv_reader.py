6c526b4ab414b8d34ebd2fd45acc92b85721b28b
Joe <joe@preferred.jp>
2022-02-27 00:24:21 +0900

    Support coincheck

--- crykak/csv_reader.py
+++ crykak/csv_reader.py
@@ -1,5 +1,5 @@
 import csv
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple
 
 from crykak.periodic_averaged import PeriodicAveraged
 
@@ -14,8 +14,51 @@ def parse_bitbank_csv_line(line: Dict[str, str]) -> Tuple[str, str, int, float, 
     return currency, buy_or_sell, year, rate, amount, charge
 
 
-def parse_coincheck_csv_line(line: Dict[str, str]) -> Tuple[str, str, int, float, float, float]:
-    raise NotImplementedError()
+def parse_coincheck_csv_line(
+    line: Dict[str, str]
+) -> Optional[Tuple[str, str, int, float, float, float]]:
+    operation = line["operation"]
+    assert operation in [
+        "Buy",
+        "Credit Card Purchase",
+        "Sell",
+        "Completed trading contracts",
+        "Limit Order",
+        "Cancel Limit Order",
+        "Transfer",
+        "Sent",
+        "Received",
+        "Bank Withdrawal",
+    ], f"Unknown operation: {operation}"
+
+    year = int(line["time"].split("-")[0])
+    amount = float(line["amount"])
+    currency = line["trading_currency"].lower()
+    original_currency = line["original_currency"].lower()
+    comment = line["comment"]
+    if operation in ["Buy", "Credit Card Purchase"]:
+        price = float(line["price"])
+        assert amount > 0 and price < 0 and original_currency == "jpy"
+        price = abs(price)
+        return currency, "buy", year, price / amount, amount, 0
+    elif operation == "Sell":
+        price = float(line["price"])
+        assert amount < 0 and price > 0 and original_currency == "jpy"
+        amount = abs(amount)
+        return currency, "sell", year, price / amount, amount, 0
+    elif operation == "Completed trading contracts":
+        assert "Rate: " in comment and "Pair: " in comment
+        crypto, jpy = comment.split("Pair: ")[1].split("_")
+        assert jpy == "jpy"
+        rate = float(comment.split("Rate: ")[1].split(",")[0])
+        if currency == crypto:
+            return crypto, "sell", year, rate, amount, 0
+        else:
+            assert currency == "jpy"
+            assert rate > 0
+            return crypto, "buy", year, rate, amount / rate, 0
+    else:
+        return None
 
 
 def register_from_csv(manager: PeriodicAveraged, csv_file: str) -> None:
@@ -42,7 +85,10 @@ def register_from_csv(manager: PeriodicAveraged, csv_file: str) -> None:
             parse_func = (
                 parse_bitbank_csv_line if csv_type == "bitbank" else parse_coincheck_csv_line
             )
-            currency, buy_or_sell, year, rate, amount, charge = parse_func(line)
+            parsed = parse_func(line)
+            if parsed is None:
+                continue
+            currency, buy_or_sell, year, rate, amount, charge = parsed
 
             if buy_or_sell == "buy":
                 manager.register_buy(currency, year, rate, amount, charge)
